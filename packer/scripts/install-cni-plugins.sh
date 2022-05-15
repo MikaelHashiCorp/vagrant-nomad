a2078f19ebdf0094ce0fde9cce2015a613fe65d3
Mikael Sikora <mikael.sikora@hashicorp.com>
2022-05-15 10:24:17 -0700

    Make it re-runnable

--- packer/scripts/install-cni-plugins.sh
+++ packer/scripts/install-cni-plugins.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -d /opt/cni/bin ]; then
-  return
-fi
+# if [ -d /opt/cni/bin ]; then
+#   return
+# fi
 
 version="0.8.1"
 url="https://github.com/containernetworking/plugins/releases/download/v${version}/cni-plugins-linux-amd64-v${version}.tgz"
